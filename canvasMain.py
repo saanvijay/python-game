0d73321ce08b940d663cf2faa693e46b1a66819d
saanvijay <mvjp.183@gmail.com>
2017-08-13 08:58:03 +0530

    Added more objects with numbers

--- canvasMain.py
+++ canvasMain.py
@@ -1,24 +1,46 @@
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
+
 import sys
+from random import randint
 
 class gameCanvas(QWidget):
 	def __init__(self, *args):
 		QWidget.__init__(self, *args)
 		canvasLayout = QFormLayout()
-		#canvasLayout.addWidget(QLabel('Welcome to Python game'))
-		#self.setLayout(canvasLayout)
+		self.setLayout(canvasLayout)
+		self.timer = QTimer()
+		self.timer.start(100)
+
+	def mousePressEvent(self, QMouseEvent):
+		clickPos = QMouseEvent.pos()
+		print "clickPos", clickPos
+
+	def mouseReleaseEvent(self, QMouseEvent):
+		cursorPos = QCursor().pos()
+		print "cursorPos", cursorPos
 
 	def paintEvent(self, event):
 		painter = QPainter(self)
 		painter.setPen(QPen(QColor(255,0,0)))
-		painter.setBrush(Qt.SolidPattern)
-		painter.drawEllipse(10,10, 50, 50)
-
+		
+		for i in xrange(255):
+			self.timer.start(100)
+			painter.setBrush(QColor(randint(0,255),randint(0,255),randint(0,255)))
+			xPos = randint(0,750)
+			yPos = randint(0,550)
+			painter.drawEllipse(xPos,yPos, 50, 50)
+			txPos = xPos + 15
+			tyPos = yPos + 30
+			painter.setPen(QPen(QColor(0,0,0)))
+			painter.drawText(int(txPos), int(tyPos), str(i))
+			self.timer.stop()
 
 if __name__ == '__main__':
 	application = QApplication(sys.argv)
 	canvas = gameCanvas()
-	canvas.resize(400, 300)
+	canvas.resize(800, 600)
+	canvas.setStyleSheet('background:darkCyan')
+	canvas.setWindowTitle('Python Game')
 	canvas.show()
 	application.exec_()
